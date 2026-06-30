6dc6d720b28039739c1b2cd04d8b8b0c612de7f6
Salman Khan <imsk2512@gmail.com>
2026-06-30 22:16:49 +0530

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,10 +86,32 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
-  },
+  icon: [
+    { url: "/favicon.ico" },
+    {
+      url: "/favicon-16x16.png",
+      sizes: "16x16",
+      type: "image/png",
+    },
+    {
+      url: "/favicon-32x32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
+    {
+      url: "/favicon-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+  ],
+  shortcut: "/favicon.ico",
+  apple: [
+    {
+      url: "/apple-touch-icon.png",
+      sizes: "180x180",
+    },
+  ],
+},
 };
 
 export const viewport: Viewport = {
